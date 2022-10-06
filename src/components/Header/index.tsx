ef6862cf9e839a2c635c27546406289cd3c1e464
rodrigo <jrodrigo0592@gmail.com>
2022-10-06 13:10:01 -0300

    ✨ Creating timer page

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { HeaderContainer } from './styles'
+
+import LogoIgnite from '../../assets/ignite-logo.svg'
+import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+
+function Header() {
+  return (
+    <HeaderContainer>
+      <img src={LogoIgnite} alt="" />
+      <nav>
+        <NavLink to="/" end title="timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="history">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
+
+export { Header }
---
+++ src/layouts/DefaultLayout/Index.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom'
+import { Header } from '../../components/Header'
+import { LayoutContainer } from './styles'
+
+function Index() {
+  return (
+    <LayoutContainer>
+      <Header />
+      <Outlet />
+    </LayoutContainer>
+  )
+}
+
+export { Index }
