ae33b5a00cb7ef2e43c615f4ce92b46922d30def
rodrigo <jrodrigo0592@gmail.com>
2022-10-07 12:12:49 -0300

    Creating Homepage

---
+++ src/pages/Home/Index.tsx
@@ -0,0 +1,61 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
+
+function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+            id="task"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="projeto1"></option>
+            <option value="projeto2"></option>
+            <option value="projeto3"></option>
+            <option value="projeto4"></option>
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            placeholder="00"
+            id="minutesAmount"
+            type="number"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
+
+export { Home }
